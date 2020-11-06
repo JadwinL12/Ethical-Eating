971f9a541f4d3791033901309e7e8431d4e5513d
ohch00 <55716852+ohch00@users.noreply.github.com>
2020-11-06 11:07:17 -0800

    added functions used for ethics page

--- public/stylesheets/ingredients.js
+++ public/stylesheets/ingredients.js
@@ -63,6 +63,113 @@ window.addEventListener("load", function() {
     addIngredient();
 })
 
+/* For ethics page, to be merged with ingredients/meals page eventually*/
+var food = `[
+  {
+      "name": "Brocolli",
+      "type": "Vegetable",
+      "ethicRank": 1
+  },
+  {
+      "name": "Lettuce",
+      "type": "Vegetable",
+      "ethicRank": 1
+  },
+  {
+      "name": "Cauliflower",
+      "type": "Vegetable",
+      "ethicRank": 1
+  },
+  {
+      "name": "Spinach",
+      "type": "Vegetable",
+      "ethicRank": 1
+  },
+  {
+      "name": "Chicken",
+      "type": "Meat",
+      "ethicRank": 1
+  },
+  {
+      "name": "Beef",
+      "type": "Meat",
+      "ethicRank": 1
+  },
+  {
+      "name": "Pork",
+      "type": "Meat",
+      "ethicRank": 1
+  },
+  {
+      "name": "Turkey",
+      "type": "Meat",
+      "ethicRank": 1
+  },
+  {
+      "name": "Butter",
+      "type": "Dairy",
+      "ethicRank": 1
+  },
+  {
+      "name": "Cream",
+      "type": "Dairy",
+      "ethicRank": 1
+  },
+  {
+      "name": "Cow Milk",
+      "type": "Dairy",
+      "ethicRank": 1
+  },
+  {
+      "name": "Almond Milk",
+      "type": "Dairy",
+      "ethicRank": 1
+  },
+  {
+      "name": "Oat Milk",
+      "type": "Dairy",
+      "ethicRank": 1
+  }
+]`
 
+var mydata = JSON.parse(food);
+var alt = document.getElementById("alts");
 
+/* generates alternative ingredients based on food type, to be optimized later, so there aren't multiple functions that do the same thing*/
+function altGeneratorMeat (){
+  removeAlts()
+  for (i=0; i < mydata.length; i++){
+    var obj = mydata[i];
+    if (obj.type == "Meat"){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(obj.name));
+    alt.appendChild(li);
+    }
+}}
 
+function altGeneratorDairy (){
+  removeAlts()
+  for (i=0; i < mydata.length; i++){
+    var obj = mydata[i];
+    if (obj.type == "Dairy"){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(obj.name));
+    alt.appendChild(li);
+    }
+}}
+
+function altGeneratorVeggies (){
+  removeAlts()
+  for (i=0; i < mydata.length; i++){
+    var obj = mydata[i];
+    if (obj.type == "Veggies"){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(obj.name));
+    alt.appendChild(li);
+    }
+}}
+
+function removeAlts(){
+  var alt_ul = document.getElementById("alts");
+  alt_ul.textContent = '';
+}
