4311448ffeaf78e9a699a23ad093e6ab47ef3c17
chunhochan <chanc3@oregonstate.edu>
2020-11-22 00:44:22 +0800

    Saved Recipes code smell
    Bloaters: save all ing in list

--- public/stylesheets/savedRecipes.js
+++ public/stylesheets/savedRecipes.js
@@ -1,14 +1,36 @@
-display = true
-function showPopup(link){
-	if(display){
-		document.getElementById(link).style.display="block";
-      display = false;
-    }else{
-    	document.getElementById(link).style.display="none";
-          display = true;
-    }
-}
-
 function togglePopup(){
   document.getElementById("popup-1").classList.toggle("active");
 }
+
+
+var Carbonara = ["Virgin Olive Oil", "Pancetta", "Garlic", "Eggs", "Spaghetti", "Salt", "Black Pepper"]
+var HawaiianPizza = ["Olive Oil", "Chees"]
+var BarbecueRibs = ["Pork ribs","Barbecue sauce","Salt"]
+
+var addedRecipe = ["Carbonara","HawaiianPizza","BarbecueRibs"]
+
+
+function showIng(data){
+  var outString=""
+  for (var i=0; i<data.length; i++){
+    console.log(data[i])
+    outString += "<li>" + data[i] + "</li>"
+  }
+  document.getElementById("output1").innerHTML = outString;
+
+}
+
+
+
+function showList(data){
+  var SavedRecipeList=""
+  for (var i=0; i<data.length; i++){
+    console.log(data[i])
+    SavedRecipeList += "<h3>" + data[i] + "<button onclick=showIng("+ data[i] + ")> + </button></h3>" 
+    
+  }
+  document.getElementById("SavedRecipeList").innerHTML = SavedRecipeList;
+
+}
+
+showList(addedRecipe)
