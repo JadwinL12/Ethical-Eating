25ec9a615fe3c3fb9b482fcc85bd130e691df4f1
Josh-Sanford <sanfojos@oregonstate.edu>
2020-11-22 16:23:03 -0800

    Added meals.js and set up skeleton code.

---
+++ public/stylesheets/meals.js
@@ -0,0 +1,20 @@
+let categories = ['Soups', 'Salads', 'Sandwiches/Wraps']
+
+let soups = {'Tomato Soup': ['Olive Oil', 'Yellow Onion', 'Tomato Paste', 'etc.'],
+    'Clam Chowder': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Potato Soup': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Tortilla Soup': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
+
+let salads = {'Greek Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Caesar Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Cobb Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Coleslaw': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
+
+let sandwichesAndWraps = {'Reuben': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Spinach Wrap': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Bacon Lettuce Tomato': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+    'Chicken Wrap': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
+
+function generateAccordionElements() {
+    
+}
