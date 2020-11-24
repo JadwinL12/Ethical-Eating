e3c2a5200aa4b2a2c8a03e74f275ac1012bf5b1f
Josh-Sanford <sanfojos@oregonstate.edu>
2020-11-23 18:56:47 -0800

    Changed meal categories

--- public/stylesheets/meals.js
+++ public/stylesheets/meals.js
@@ -1,20 +1,30 @@
-let categories = ['Soups', 'Salads', 'Sandwiches/Wraps']
+let categories = ['Breafast', 'Lunch', 'Dinner', 'Snacks', 'Dessert']
 
-let soups = {'Tomato Soup': ['Olive Oil', 'Yellow Onion', 'Tomato Paste', 'etc.'],
+let breakfast = {'Tomato Soup': ['Olive Oil', 'Yellow Onion', 'Tomato Paste', 'etc.'],
     'Clam Chowder': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Potato Soup': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Tortilla Soup': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
 
-let salads = {'Greek Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+let lunch = {'Greek Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Caesar Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Cobb Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Coleslaw': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
 
-let sandwichesAndWraps = {'Reuben': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
+let dinner = {'Reuben': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Spinach Wrap': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Bacon Lettuce Tomato': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
     'Chicken Wrap': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
 
-function generateAccordionElements() {
-    
+let snacks = {};
+
+let dessert = {};
+
+let breakfastMeals = document.getElementById('breakfastMeals');
+let lunchMeals = document.getElementById('lunchMeals');
+let dinnerMeals = document.getElementById('dinnerMeals');
+
+function generateBreakfast() {
+    for (let meal of breakfast) {
+
+    }
 }
