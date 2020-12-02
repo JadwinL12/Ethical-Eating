7c1f77aca28885e6210b48dc23d12c3ae495aec1
Josh-Sanford <sanfojos@oregonstate.edu>
2020-12-02 10:29:48 -0800

    Updated meals in each category

--- public/stylesheets/meals.js
+++ public/stylesheets/meals.js
@@ -1,27 +1,58 @@
-let categories = ['Breafast', 'Lunch', 'Dinner', 'Snacks', 'Dessert']
+let categories = ['Breafast', 'Lunch', 'Dinner', 'Dessert']
 
-let breakfast = {'Tomato Soup': ['Olive Oil', 'Yellow Onion', 'Tomato Paste', 'etc.'],
-    'Clam Chowder': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Potato Soup': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Tortilla Soup': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
+let breakfast = {
+    'Pancakes with Maple Syrup': ['Ingredient'],
+    'Eggs Benedict': ['Ingredient'],
+    'Biscuits and Gravy': ['Ingredient'],
+    'Bagel with Cream Cheese': ['Ingredient'],
+    'Cinnamon Rolls': ['Ingredient'],
+    'Belgian Style Waffles': ['Ingredient'],
+    'Breakfast Burrito': ['Ingredient'],
+    'Bacon, Eggs and Hashbrowns': ['Ingredient'],
+    'Breakfast Parfait': ['Ingredient'],
+    'Sausage and Egg Sandwich': ['Ingredient'],
+    'Stuffer Omelet': ['Ingredient'],
+    'Oatmeal': ['Ingredient']
+};
 
-let lunch = {'Greek Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Caesar Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Cobb Salad': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Coleslaw': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
+let lunch = {
+    'Ham and cheese sandwich': ['Ingredient'],
+    'Tuna Salad Sandwich': ['Ingredient'],
+    'Reuben': ['Ingredient'],
+    'Cheeseburger and French Fries': ['Ingredient'],
+    'Chicken Strips and French Fries': ['Ingredient'],
+    'Grilled Chicken Caesar Salad': ['Ingredient'],
+    'Enchiladas': ['Ingredient'],
+    'Sushi Rolls': ['Ingredient'],
+    'Fish Tacos': ['Ingredient']
+};
 
-let dinner = {'Reuben': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Spinach Wrap': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Bacon Lettuce Tomato': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient'],
-    'Chicken Wrap': ['Ingredient', 'Ingredient', 'Ingredient', 'Ingredient']};
+let dinner = {
+    'Meatloaf and Mashed Potatoes': ['Ingredient'],
+    'Spaghetti': ['Ingredient'],
+    'Steak and Baked Potato': ['Ingredient'],
+    'Vegetable Soup': ['Ingredient'],
+    'Chicken Parmesan': ['Ingredient'],
+    'Fish and Chips': ['Ingredient'],
+    'Pot Roast': ['Ingredient'],
+    'Jambalaya': ['Ingredient'],
+};
 
-let snacks = {};
-
-let dessert = {};
+let dessert = {
+    'Banana Pudding': ['Ingredient'],
+    'Apple Pie': ['Ingredient'],
+    'Peach Cobbler': ['Ingredient'],
+    'Chocolate Cake': ['Ingredient'],
+    'Carrot Cake': ['Ingredient'],
+    'Chocolate Chip Cookies': ['Ingredient'],
+    'Snickerdoodle Cookies': ['Ingredient'],
+    'Tiramisu': ['Ingredient'],
+};
 
 let breakfastMeals = document.getElementById('breakfastMeals');
 let lunchMeals = document.getElementById('lunchMeals');
 let dinnerMeals = document.getElementById('dinnerMeals');
+let dessertMeals = document.getElementByID('dessertMeals');
 
 function generateBreakfast() {
     for (let meal of breakfast) {
